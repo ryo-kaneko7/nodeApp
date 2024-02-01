fdfdd2491996d74725d40666d4f678f13ceebf47
ryo-kaneko7 <e53763@jp.ibm.com>
2024-02-01 22:27:08 +0900

    エラーコード物理削除

--- app.js
+++ app.js
@@ -12,7 +12,3 @@ var server = app.listen(port, function () {
 app.get("/", function (req, res, next) {
     res.send("Hello World Change!!");
 });
-
-//function unsafeFunction(userInput) {
-//    eval(userInput); // This is very dangerous and will be flagged by SonarCloud
-//}
